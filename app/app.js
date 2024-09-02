edacd87dc48f318cca340651538cfcfe33c4b8c1
luisthieme <luis.thieme@5minds.de>
2024-09-02 21:36:25 +0200

    use socket.io

--- app/app.js
+++ app/app.js
@@ -1,18 +1,18 @@
-const socket = new WebSocket('ws://localhost:3000');
+const socket = io('ws://localhost:3500');
 
 function sendMessage(e) {
     e.preventDefault();
     const input = document.querySelector('input');
     if (input.value) {
-        socket.send(input.value);
-        input.value;
+        socket.emit('message', input.value);
+        input.value = '';
     }
     input.focus();
 }
 
 document.querySelector('form').addEventListener('submit', sendMessage);
 
-socket.addEventListener('message', ({ data }) => {
+socket.on('message', (data) => {
     const li = document.createElement('li');
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
-const ws = require('ws');
-const server = new ws.Server({ port: 3000 });
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-server.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        const buf = Buffer.from(message);
-        console.log(buf.toString());
-        socket.send(`${message}`);
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === 'production' ? false : ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    },
+});
+io.on('connection', (socket) => {
+    console.log('user id: ', socket.id);
+    socket.on('message', (data) => {
+        console.log(data);
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
     });
 });
+
+httpServer.listen(3500, () => console.log('listening on port 3500'));
